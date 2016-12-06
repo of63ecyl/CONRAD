1c9b814f88c12362fc0600b005b8fb1d9fe451ca
Martin Ullrich <of63ecyl@stud.informatik.uni-erlangen.de>
2016-12-06 11:44:12 +0100

    fanogram

--- src/edu/stanford/rsl/tutorial/praktikum/FanBeam.java
+++ src/edu/stanford/rsl/tutorial/praktikum/FanBeam.java
@@ -1,46 +1,149 @@
 package edu.stanford.rsl.tutorial.praktikum;
 
+import ij.ImageJ;
+
+import java.util.ArrayList;
+
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import edu.stanford.rsl.conrad.data.numeric.InterpolationOperators;
+import edu.stanford.rsl.conrad.geometry.shapes.simple.Box;
+import edu.stanford.rsl.conrad.geometry.shapes.simple.PointND;
+import edu.stanford.rsl.conrad.geometry.shapes.simple.StraightLine;
+import edu.stanford.rsl.conrad.geometry.transforms.Translation;
+import edu.stanford.rsl.conrad.numerics.SimpleOperators;
+import edu.stanford.rsl.conrad.numerics.SimpleVector;
 
 public class FanBeam {
 	
-	//spacing - mm per pixel
-	private double detectorSpacing;
-	//number of pixels on the detector
-	private int numDetectorElements;
-	//detector length in [mm] 
-	private double detectorLength;
-	//increment of rotation angle beta between two projections in [deg]
-	private double betaIncrement;
-	//number of acquired projections
-	private int numProjs;
-	//source to isocenter distance d_si
-	private double d_si;
-	//source to detector distance d_sd
-	private double d_sd;
-	//max rotation angle beta in [deg]
-	private double maxBeta;
 	
-	public void setSinogramParams(Grid2D sino, double d_si,double d_sd, double maxRot)
-	{
-		this.d_si = d_si;
-		this.d_sd = d_sd;
-		this.numProjs = sino.getHeight();
-		this.numDetectorElements = sino.getWidth();
-		this.detectorSpacing = sino.getSpacing()[0];
-		this.detectorLength = detectorSpacing*numDetectorElements;
+public Grid2D projectRayDrivenFan(Grid2D grid, int numProjs, double detectorSpacing, int numDetectorElements, double maxBeta, double d_si, double d_sd) {
 		
-		double halfFanAngle = Math.atan((detectorLength/2.0) / d_sd);
-		System.out.println("Half fan angle: " + halfFanAngle*180.0);
-		this.maxBeta = maxRot + 2.0*halfFanAngle;
-		this.betaIncrement = maxBeta /(double) numProjs;
-		System.out.println("Short-scan range: " + maxBeta*180);
+		int maxSIndex = numDetectorElements;
+		double detectorLength = (numDetectorElements-1) * detectorSpacing; //mm
 		
+		double deltaS = detectorSpacing;
+		double betaIncrement = maxBeta / numProjs;
+		
+		final double samplingRate = 3.d; // # of samples per pixel
+		Grid2D fanogram = new Grid2D(new float[numProjs*numDetectorElements], numDetectorElements, numProjs);
+		fanogram.setSpacing(detectorSpacing, betaIncrement);
+		
+		//PointND originP = new PointND(grid.getOrigin()[0],grid.getOrigin()[1], .0d);
+
+		// set up image bounding box in WC
+		Translation trans = new Translation(
+				(grid.getOrigin()[0]), grid.getOrigin()[1], -1);
+
+		Box b = new Box((grid.getSize()[0] * grid.getSpacing()[0]), (grid.getSize()[1] * grid.getSpacing()[1]), 2);
+		b.applyTransform(trans);
+
+		for(int e=0; e<numProjs; ++e){
+			// compute beta [rad] and angular functions.
+			double beta = (betaIncrement * e * Math.PI)/180;
+			double cosBeta = Math.cos(beta);
+			double sinBeta = Math.sin(beta);
+			PointND sourceP = new PointND (-d_si * sinBeta, d_si * cosBeta, .0d);
+			SimpleVector sourceVec = new SimpleVector(sourceP.getAbstractVector());
+			sourceVec.dividedBy(d_si).multipliedBy(-(d_sd-d_si)); //vector between source and detector center (central ray)
+			PointND sPoint = new PointND(deltaS * cosBeta, deltaS * sinBeta, .0d);
+			SimpleVector sVector = new SimpleVector(sPoint.getAbstractVector());
+			SimpleVector rayVec = new SimpleVector(sourceVec);
+			rayVec.subtract(sVector.multipliedBy(detectorLength / 2));
+			
+			for (int i = 0; i < maxSIndex; ++i) { 
+				// compute s, the distance from the detector edge in WC [mm]
+				rayVec.add(sVector.multipliedBy(i*deltaS));
+				
+				PointND rayP = new PointND(rayVec.getElement(0),rayVec.getElement(1), .0d);
+
+				
+				// set up line equation
+				StraightLine line = new StraightLine(sourceP, rayP);
+				// compute intersections between bounding box and intersection line.
+				ArrayList<PointND> points = b.intersect(line);
+
+				/*// only if we have intersections
+				if (2 != points.size()){
+					if(points.size() == 0) {
+						line.getDirection().multiplyBy(-1.d);
+						points = b.intersect(line);
+					}*/
+					if(points.size() != 2){
+						continue;
+				}
+
+				PointND start = points.get(0); // [mm]
+				PointND end = points.get(1);   // [mm]
+				
+				SimpleVector startVec = new SimpleVector(start.getAbstractVector());
+				//start = inverse.transform(start);
+				
+				PointND current = new PointND(startVec);
+				
+				// compute once for start point
+				double [] indices = grid.physicalToIndex(current.get(0), current.get(1));				
+				double sum = InterpolationOperators.interpolateLinear(grid, indices[0], indices[1]);
+				
+				
+				
+				SimpleVector endVec = new SimpleVector(end.getAbstractVector());
+				SimpleVector integralVec = SimpleOperators.subtract(endVec, startVec);
+				double distance = integralVec.normL2();
+				integralVec.divideBy(distance * samplingRate);
+				
+				
+				for (double t = 0.0; t < (distance * samplingRate)-1; t++){
+					current.getAbstractVector().add(integralVec);
+					
+					indices = grid.physicalToIndex(current.get(0), current.get(1));
+					
+					if (grid.getSize()[0] <= indices[0] + 1 || grid.getSize()[1] <= indices[1] + 1 || indices[0] < 0 || indices[1] < 0){
+						continue;
+					}
+					
+					sum += InterpolationOperators.interpolateLinear(grid, indices[0], indices[1]);
+				}	
+								
+
+				// normalize by the number of interpolation points
+				sum /= samplingRate;
+				
+				
+				// write integral value into the sinogram.
+				fanogram.setAtIndex(i, e, (float)sum);
+			}
+		}
+		return fanogram;
 	}
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new ImageJ();
+		double [] spacing = {1,1};
+		Phantom phantom = new Phantom(512,512,spacing);
+		int sizeX = phantom.getSize()[0];
+		int sizeY = phantom.getSize()[1];
+		phantom.show("The phantom");
+		
+		FanBeam fan = new FanBeam();
+		
+		// number of projection images	
+		int projectionNumber = 180;	
+		// detector size in pixel
+		int detectorSize = 512; 
+		// size of a detector Element [mm]
+		double detectorSpacing = 1.0f;
+		
+		double d_si = 512;
+		double d_sd = 1024;
+		double halfFanAngle = Math.atan((detectorSize/2.0)/d_sd);
+		double maxBeta = 180 + 2 * halfFanAngle;
+		
+				
+				
+		Grid2D fanogram = fan.projectRayDrivenFan(phantom, projectionNumber, detectorSpacing, detectorSize, maxBeta, d_si, d_sd); 
+		fanogram.show("The Unfiltered Sinogram");
 
 	}
 
