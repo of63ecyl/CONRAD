7e2850f8aa136627dc1e2b9bd927605b8b9d79a4
of63ecyl <martin.ullrich@fau.de>
2017-02-28 09:40:20 +0100

    finish all

---
+++ src/edu/stanford/rsl/tutorial/praktikum/coneBeam.java
@@ -0,0 +1,20 @@
+package edu.stanford.rsl.tutorial.praktikum;
+import java.io.File;
+import java.util.Scanner;
+
+import ij.ImageJ;
+import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+
+public class coneBeam {
+
+	public static void main(String[] args) {
+/*		// TODO Auto-generated method stub
+		File file = new File("/proj/i5fpctr/newData/Pig Head/projtable_leftSide.txt");
+		Scanner scanner = new Scanner(file);*/
+		new ImageJ();
+		
+		
+
+	}
+
+}
